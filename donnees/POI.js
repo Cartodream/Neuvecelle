2f5378b00f3e0b093c6c0f98c79d1d826ae1ef7b
Cartodream <90085887+Cartodream@users.noreply.github.com>
2021-11-08 15:53:28 +0100

    Add files via upload

---
+++ donnees/POI.js
@@ -0,0 +1,15 @@
+var poi = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+
+
+{ "type": "Feature", "properties": { "id": "ARTBOU0210021133", "nom": "Eglise de Maraîche", "descriptif": null, "tel": null, "mail": null, "site_web": "", "sous_cat": "Patrimoine religieux", "categorie": "Patrimoine culturel", "adresse": "142 Avenue d'Abondance", "adresse2": "", "cp": "74500", "commune": "Neuvecelle", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [ 6.614064596457445, 46.39790792867803 ] } },
+{ "type": "Feature", "properties": { "id": "ARTBOU0210021134", "nom": "Clos Gemme Plus", "descriptif": null, "tel": null, "mail": null, "site_web": "http://www.gemme-plus.com/", "sous_cat": "Chambre d'hôtes", "categorie": "Commerces", "adresse": "437 Avenue du Flon", "adresse2": "", "cp": "74500", "commune": "Neuvecelle", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [ 6.607256462240141 ,46.39650231968192 ] } },
+
+
+
+{ "geometry": {"type": "Point", "coordinates": [6.612378061613508, 46.39534144050666]},"type": "Feature", "properties": {"nom": "Mairie", "descriptif" : "Heures d'ouverture : Du Mardi au Vendredi de 14h30 à 18h30, le Mercredi et le Samedi de 9h à 12h30", "tel": "33 (0)4.50.75.01.77", "commune": "Neuvecelle", "photo": null, "sous_cat": "Mairie", "adresse": "42 Avenue de Verlagny" , "categorie": "Bâtiment Public", "source": null, "site_web": null, "mail": "", "cp": "74500", "id": 245}}
+]
+};
